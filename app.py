93a2c7a58913901882bd440df7c4632ff354f9ad
ricardooandree <ricardooandree@gmail.com>
2024-01-28 16:44:06 +0000

    Working routes /, /login, /logout, /register without DB

--- app.py
+++ app.py
@@ -16,6 +16,15 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
@@ -28,23 +37,90 @@ def login_required(f):
 
 @app.route("/")
 @login_required
-def index():
-    return render_template("index.html")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    return render_template("login.html")
+    """Log user in"""
+    
+    # Forget any user_id
+    session.clear()
+    
+    # User reached route via POST
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        # Input validation
+        if not username:
+            flash("Username is empty", "error")
+            return render_template("homepage.html")
+        
+        elif not password:
+            flash("Password is empty", "error")
+            return render_template("homepage.html")
+        
+        # Query database for username
+        # Ensure username exists and password is correct
+        
+        # Remember which user has logged in
+        session["user_id"] = "admin"    # TEST SESSION
+        
+        # Redirect user to home page
+        return redirect("/")
+    
+    # User reacher route via GET
+    else:
+        return render_template("homepage.html")
 
 
 @app.route("/logout")
 def logout():
+    # Forget any user_id
+    session.clear()
+    
     return redirect("/")
 
 
 @app.route("/register", methods = ["GET", "POST"])
 def register():
-    return render_template("register.html")
+    """Register a new user"""
+    
+    # User reached route via POST
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        
+        # Input validation
+        if not username:
+            flash("Username is empty", "error")
+            return render_template("register.html")
+        
+        elif not password:
+            flash("Password is empty", "error")
+            return render_template("register.html")
+        
+        elif not confirmation:
+            flash("Password confirmation is empty", "error")
+            return render_template("register.html")
+        
+        # Query database for username
+        # Ensure username is available 
+        # Insert user into database
+        # Query database for user_id    
+
+        # Remember which user has logged in
+        session["user_id"] = "admin"    # TEST SESSION
+        
+        # Redirect user to home page #
+        return redirect("/")
+    
+    # User reacher route via GET
+    else:
+        return render_template("register.html")
 
 
 @app.route("/profile", methods = ["GET", "POST"])
