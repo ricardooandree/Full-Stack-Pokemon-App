e636426be83ddd0f69d7e8f02e74037df8219cb3
ricardooandree <ricardooandree@gmail.com>
2024-02-10 21:49:44 +0000

    Deployment attempt

--- app.py
+++ app.py
@@ -820,4 +820,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create tables before running the app
     #app.run(debug=True) # Development mode
-    app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    #app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    app.run(threaded=True)
